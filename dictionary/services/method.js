b4bf16c9b0d172041489bd035f7d24d0f9d45bf2
Chandra Shekar Reddy <dubbachandra.shekarreddy@scopeworker.com>
2019-11-10 15:43:38 +0530

    Synonyms, Antonyms, Examples Added

--- dictionary/services/method.js
+++ dictionary/services/method.js
@@ -1,6 +1,5 @@
-const axios = require('axios')
 const readline = require('readline')
-const http = require('https')
+const request = require('request-promise')
 const config = require('../config/sample.json')
 
 const rl = readline.createInterface({
@@ -10,25 +9,88 @@ const rl = readline.createInterface({
 
 let index = (type,word) => {
     console.log(type,word,config)
-    definition(word)
+
+    switch(type){
+        case "defn":
+            definitions(word)
+            break
+        case "syn":
+            synonyms(word)
+            break
+        case "ant":
+            antonyms(word)
+            break
+        case "ex":
+            examples(word)
+            break
+    
+    }
 }
 
 let getData = async (url) => {
-    return axios({
-        url: url,
-        baseURL: config.host,
-        method: 'get'
-      })
+    let request_params = {
+        url : url,
+        method : "GET"
+    }
+
+    let request_output = await request(request_params)
+
+    return JSON.parse(request_output)
 }
 
-let definition = async (word) => {
-    let url = `/word/${word}/definitions?api_key=` + config.key
+let definitions = async (word) => {
+    let url = config.host + `/word/${word}/definitions?api_key=` + config.key
 
-    await getData(url).then(response => console.log(response.data)).catch(err => {console.log('Word Not Found in Dictionary')})
+    await getData(url).then(response => console.log(response)).catch(err => {console.log('Word Not Found in Dictionary')})
     
     return Promise.resolve()
 }
 
+let synonyms = async (word) => {
+    let url = config.host + `/word/${word}/relatedWords?api_key=` + config.key
+
+    await getData(url)
+    .then(response => processData(response,'syn'))
+    .then(response => console.log(response))
+    .catch(err => {console.log('Word Not Found in Dictionary',err)})
+    
+    return Promise.resolve()
+}
+
+let antonyms = async (word) => {
+    let url = config.host + `/word/${word}/relatedWords?api_key=` + config.key
+
+    await getData(url)
+    .then(response => processData(response,'ant'))
+    .then(response => console.log(response))
+    .catch(err => {console.log('Word Not Found in Dictionary',err)})
+    
+    return Promise.resolve()
+}
+
+let examples = async (word) => {
+    let url = config.host + `/word/${word}/examples?api_key=` + config.key
+
+    await getData(url)
+    .then(response => console.log(response.examples))
+    .catch(err => {console.log('Word Not Found in Dictionary',err)})
+    
+    return Promise.resolve()
+}
+
+
+let processData = async (response,type) => {
+    if(type == 'syn')
+      response = response.filter(function (pilot) {
+        return pilot.relationshipType === "synonym";
+      })
+    else if(type == 'ant')
+      response = response.filter(function (pilot) {
+        return pilot.relationshipType === "antonym";
+      })
+
+    return Promise.resolve(response[0].words)
+}
 
 module.exports = {
     index
